9bc24f90a55e36e807a66109d9d438c0740040c0
Sydney <121275308+kropsyd23@users.noreply.github.com>
2025-09-24 20:30:40 +0000

    Cleaned up code and added more detailed comments

--- guessing_game.py
+++ guessing_game.py
@@ -1,49 +1,54 @@
 import random
+import main
+
 def play_game():
     """
-    Runs one session of the guessing game.
-    The computer chooses a random number between 1 and 100.
-    The player has 5 tries to guess the number correctly.
-    After each guess, the program tells the player if the guess
-    was too high or too low. If the player runs out of tries, the
-    correct number is revealed.
-    Authors name = Elayne Vilela
+    Play rock, paper, scissors with the user until they say they want to stop.
+    Author: Sydney
     """
-    play_again = "y"
-    while play_again == "y":
-        secret_number = random.randint(1, 100)
-        tries = 5
-        guessed = False
 
-        print("I'm thinking of a number between 1 and 100.")
-        guess = int(input("Guess what it is. You have " + str(tries) + " tries: "))
+    # set initial decision to 'y' because they just chose this game in the main class
+    decision = 'y'
 
-        while tries > 0 and not guessed:
-            if guess == secret_number:
-                print("You got it!", end=" \n")
-                guessed = True
-            else:
-                tries -= 1
-                if tries == 0:
-                    print("Nope! You lost. The number was " + str(secret_number), end=" \n")
-                elif guess < secret_number:
-                    guess = int(input("Nope! Too low. Try again (" + str(tries) + " tries left): "))
-                else:
-                    guess = int(input("Nope! Too high. Try again (" + str(tries) + " tries left): "))
+    # keep running the loop while the user keeps saying 'yes'
+    while (decision == 'y'):
 
-        print("Do you want to play the guessing game again? (Y/N): ", end="")
-        play_again = input().strip().lower()
-    
-    print("Thanks for playing!") #When the loop ends (user typed "n"), print the goodbye message
+        # user makes their selection
+        print('Enter your choice: 1 rock, 2 paper, 3 scissors: ', end='')
+        num_user = main.get_decision_range('Enter your choice: 1 rock, 2 paper, 3 scissors', 1, 3)
 
+        # computer makes its selection
+        num_computer = random.randint(1, 3)
 
-def main():
-    """
-    Entry point for the guessing game when run directly.
-    """
-    play_game()
+        # calculate winner (1 beats 3, 2 beats 1, 3 beats 2)
+        if (num_user == 1):
+            if (num_computer == 1):
+                print('I chose rock. It is a tie!')
+            elif (num_computer == 2):
+                print('Paper beats rock. You lose!')
+            elif (num_computer == 3):
+                print('Rock beats scissors. You win!')
+
+        elif (num_user == 2):
+            if (num_computer == 1):
+                print('Paper beats rock. You win!')
+            elif (num_computer == 2):
+                print('I chose paper. It is a tie!')
+            elif (num_computer == 3):
+                print('Scissors beats paper. You lose!')
+
+        elif (num_user == 3):
+            if (num_computer == 1):
+                print('Rock beats scissors. You lose!')
+            elif (num_computer == 2):
+                print('Scissors beats paper. You win!')
+            elif (num_computer == 3):
+                print('I chose scissors. It is a tie!')
+
+        # ask the user if they want to play Rock-Paper-Scissors again
+        print('Do you want to play Rock-Paper-Scissors again? (y/n): ', sep='', end='')
+        decision = main.get_decision('Do you want to Rock-Paper-Scissors again? (y/n)', 'y', 'n')
+        print()
 
 if __name__ == "__main__":
-
-    print('From guessing_game')
-
+    play_game()
--- main.py
+++ main.py
@@ -8,65 +8,80 @@ Date: 09/18/2025
 import rock_paper_scissors
 import guessing_game
 
+def get_value(text):
+    """
+    Returns the input as an integer if the string can be turned into an integer, and a string otherwise.
+    Author: Sydney
+    """
+
+    try:
+        return int(text)
+    except:
+        return text
+
 def get_decision_range(message, min, max):
-    '''Returns the decision of the user when the valid input is a range of integers.
+    """
+    Returns the decision of the user when the valid input is a range of integers.
     Checks if the user inputted an integer in the range [min, max].
     If input was not valid, continue prompting the user to try again until it is valid.
-    Author: Sydney'''
+    Author: Sydney
+    """
+
+    num = input()
 
     # continue prompting the user to try again until they enter a valid input
-    num = input()
     matches = False
     while (matches == False):
-        # if the input can be turned to an integer without error, check if it is in the correct range
         try:
-            num = int(num)
+            # num will be converted into an integer if an integer was inputted, otherwise remain a string
+            num = get_value(num)
+            # check if the input is in [min, max]
+            # will cause an error if the input was not an integer to enter the catch
             if (num >= min and num <= max):
                 matches = True # change matches to True to exit the while loop
+
             # if it is not between 1 and 100, raise an error to enter the catch
             else:
                 raise ValueError()
 
-        # if the input was not an integer or an integer outside of [1, 100], prompt the user to try again
+        # if the input was not an integer or an integer outside of [min, max], prompt the user to try again
         except:
             print('Please enter a valid input! ', message, ': ', sep='', end='')
             num = input()
 
     return num
 
-def get_value(text):
-    '''Returns the input as an integer if the string can be turned into an integer, and a string otherwise.
-    Author: Sydney'''
-    try:
-        return int(text)
-    except:
-        return text
-
 def get_decision(message, *args):
-    '''Returns the decision of the user. Checks if the user enters an invalid input based on parameters
-    provided and prompts the user to try again until they enter a valid input.
-    Author: Sydney'''
+    """
+    Checks if the user enters an input that matches the parameters provided.
+    Prompts the user to try again until they enter a valid input.
+    Returns the decision of the user once it is a valid input.
+    Author: Sydney
+    """
+
     text = get_value(input())
     matches = False
 
-    # check if the input initially passed matches one of the possible options
-    for n in args:
-        if (text == n):
-            matches = True
-
-    # if it doesn't match, continue prompting for a new input until it matches
+    # remain in this loop until the input matches one of the valid inputs
     while (matches == False):
-        print('Please enter a valid input! ', message, ': ', sep='', end='')
-        text = get_value(input())
+        # check if the input matches any of the valid inputs
         for n in args:
             if (text == n):
                 matches = True
 
+        # if the input is invalid, send an error message and take a new value
+        if (matches == False):
+            print('Please enter a valid input! ', message, ': ', sep='', end='')
+            text = get_value(input())
+
     return text
 
 def main():
-    '''Let the user choose which game they want to play. Repeat until they don't want to play anymore.
-    Author: Sydney and Elayne'''
+    """
+    Let the user choose which game they want to play.
+    Repeat until they don't want to play anymore.
+    Author: Sydney and Elayne
+    """
 
     # welcome message
     print('Welcome! You will get the chance to play two different games as many times as you want!')
